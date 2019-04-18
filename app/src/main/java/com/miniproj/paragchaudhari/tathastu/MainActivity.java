5af7f05b5c08e39dce5a0e8589d9b9e84a8b6c35
atharva <atharva.lord10@gmail.com>
2019-04-18 21:20:25 +0530

    Camera Feature Added

--- app/src/main/java/com/miniproj/paragchaudhari/tathastu/MainActivity.java
+++ app/src/main/java/com/miniproj/paragchaudhari/tathastu/MainActivity.java
@@ -1,5 +1,4 @@
 package com.miniproj.paragchaudhari.tathastu;
-//vastu corp - > boycott parag chaudhari
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button,button4,button3;
     private Button button2;
+    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,11 +52,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button5 = (Button) findViewById(R.id.button101);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opencam();
+            }
+
+        });
+
     }
     public void arview(View view)
     {
         Intent aractivity = new Intent(MainActivity.this,ArActivity.class);
         startActivity(aractivity);
+    }
+
+    public void opencam()
+    {
+        Intent intent = new Intent(this,Cam.class);
+        startActivity(intent);
     }
 
     public void openknowledge(){
