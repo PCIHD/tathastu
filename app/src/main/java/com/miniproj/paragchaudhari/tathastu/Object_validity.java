01130efc326d00d742ba00de777709e2642a1928
atharva <atharva.lord10@gmail.com>
2019-04-19 20:14:55 +0530

    Hagrat kam hopefully nighala hai and the shit working now

---
+++ app/src/main/java/com/miniproj/paragchaudhari/tathastu/Object_validity.java
@@ -0,0 +1,31 @@
+package com.miniproj.paragchaudhari.tathastu;
+/*
+1-Door
+2-Window
+
+
+Return 1 : optimal
+       2 : ok
+       3:  unacceptable
+
+
+*/
+ */
+public class Object_validity {
+
+
+    public int check_validation(int object ,  int degree){
+
+        switch(object){
+            case 1://enter code for door
+                return;
+            case 2://enter   code for window
+                    return;
+
+            default:
+                return;
+        }
+
+    }
+
+}
