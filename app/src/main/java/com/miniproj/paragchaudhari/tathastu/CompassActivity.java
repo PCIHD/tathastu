b5188907fe0f08ec78b7eea15c8b9e643069bb42
Parag Chaudhari <parag.chaudharis@gmail.com>
2019-04-19 12:15:00 +0530

    Image Capture Working

--- app/src/main/java/com/miniproj/paragchaudhari/tathastu/CompassActivity.java
+++ app/src/main/java/com/miniproj/paragchaudhari/tathastu/CompassActivity.java
@@ -1,16 +1,26 @@
 package com.miniproj.paragchaudhari.tathastu;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.ar.core.Frame;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class CompassActivity extends AppCompatActivity {
     private Custom_arFragment fragment;
+    private Button door_button,window_button;
+    SnackbarHelper snackbarHelper = new SnackbarHelper();
 
     private static final String TAG = "CompassActivity";
 
@@ -27,7 +37,20 @@ public class CompassActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compass);
         fragment = (Custom_arFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
         fragment.getPlaneDiscoveryController().hide();
-
+        door_button = (Button) findViewById(R.id.Compass_doorbutton);
+        window_button=(Button) findViewById(R.id.Compass_windowButton);
+        door_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takeDoorPhoto();
+            }
+        });
+        window_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takeWindowPhoto();
+            }
+        });
         sotwFormatter = new SOTWFormatter(this);
 
         arrowView = findViewById(R.id.main_image_hands);
@@ -97,4 +120,31 @@ public class CompassActivity extends AppCompatActivity {
             });
         };
     }
+    private String generateFileName(String object){
+        return  object + new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+
+
+    }
+    private void takeDoorPhoto(){
+        try{
+            Frame currentFrame = fragment.getArSceneView().getArFrame();
+            Image currentImage = currentFrame.acquireCameraImage();
+            String name = generateFileName("Door");
+            snackbarHelper.showMessage(this,name);
+        }catch (Exception exception){
+            snackbarHelper.showMessage(this,"Error Acquiring image");
+        }
+    }
+    private void takeWindowPhoto(){
+        try{
+            Frame currentFrame = fragment.getArSceneView().getArFrame();
+            Image currentImage = currentFrame.acquireCameraImage();
+            String name = generateFileName("Window");
+            snackbarHelper.showMessage(this,name);
+        }catch (Exception exception){
+            snackbarHelper.showMessage(this,"Error Acquiring image");
+        }
+    }
+
+
 }
